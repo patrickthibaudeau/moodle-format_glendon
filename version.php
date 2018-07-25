167c27a3dd43e405faded48cbd4a9234b03cab65
Patrick Thibaudeau <patrick.thibaudeau@ualberta.ca>
2018-07-25 12:08:30 -0400

    Update version.php
    Updated require version to Moodle 3.5

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2018072401;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires  = 2017051500;        // Requires this Moodle version.
-$plugin->release = '2.5.1 Build(2018072401)';
+$plugin->version   = 2018072500;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->requires  = 2018051700;        // Requires this Moodle version.
+$plugin->release = '2.5.2 Build(2018072500)';
 $plugin->component = 'format_glendon';    // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
     'atto_etitle' => 2017072500
