4419386b8fff66a41ccfc0ee9ba37056e525e9d3
Patrick Thibaudeau <thibaud@yorku.ca>
2018-08-10 09:48:02 -0400

    Removed Course progress help

--- version.php
+++ version.php
@@ -26,9 +26,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version   = 2018081000;        // The current plugin version (Date: YYYYMMDDXX).
+$plugin->version   = 2018081001;        // The current plugin version (Date: YYYYMMDDXX).
 $plugin->requires  = 2018051700;        // Requires this Moodle version.
-$plugin->release = '2.5.4 Build(2018081000)';
+$plugin->release = '2.5.5 Build(2018081001)';
 $plugin->component = 'format_glendon';    // Full name of the plugin (used for diagnostics).
 $plugin->dependencies = [
     'atto_etitle' => 2017072500
